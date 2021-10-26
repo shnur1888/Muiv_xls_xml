f12d2a810663ba9d78f7879cf1fd630f7454ef69
shnur1888 <80851093+shnur1888@users.noreply.github.com>
2021-10-26 20:52:09 +0300

    2
    2

---
+++ ConsoleApp2/ConsoleApp2/XlsToXlsx2.cs
@@ -0,0 +1,26 @@
+﻿using Spire.Xls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class XlsToXlsx2
+    {
+
+        public void ConvertToXlsxFile(string xlsPath, string destPath)
+        {
+
+
+            String dirPath = @"C:\Users\Win10_Game_OS\OneDrive\Рабочий стол\ffff\fff.xls";
+            Workbook workbook = new Workbook();
+            workbook.LoadFromFile(xlsPath);
+            workbook.SaveToFile(destPath, ExcelVersion.Version2013);
+
+
+
+        }
+
+
+    }
+}
